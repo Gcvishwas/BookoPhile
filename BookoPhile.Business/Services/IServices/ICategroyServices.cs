e6de098671712caf680038eeb43bb51b06d2c7f2
Gcvishwas <gcvishwas17@gmail.com>
2026-09-06 14:56:27 +0545

    Create Interface for Controller

---
+++ BookoPhile.Business/Services/IServices/ICategroyServices.cs
@@ -0,0 +1,14 @@
+﻿using BookoPhile.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BookoPhile.Business.Services.IServices
+{
+    public interface ICategroyServices
+    {
+        Task<Category?> GetCategoryByIdAsync(int id);
+        Task<IEnumerable<Category>> GetCategoriesAsync();
+        Task<Category> CreateCategoryAsync(Category category);
+    }
+}
